20b65f2e814223e58239d22afbb66a5abc1594ba
solidgriever <florentinbeltechi@gmail.com>
2017-05-27 16:11:06 +0300

    FP game

---
+++ hacktm/Assets/Scripts/Generate.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+
+public class Generate : MonoBehaviour
+{
+    public GameObject rocks;
+    int score = 0;
+    int timer = 30;
+
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("CreateObstacle", 1f, 1.5f);
+    }
+
+    void OnGUI()
+    {
+        Rect rect = new Rect(Screen.width - 120, 0, 100, 50);
+        GUIStyle rightStyle = GUI.skin.GetStyle("Label");
+        rightStyle.alignment = TextAnchor.UpperRight;
+        GUI.color = Color.black;
+        GUILayout.Label(" Score: " + score.ToString());        
+        GUI.Label(rect, "Time: " + timer.ToString(), rightStyle);
+    } 
+
+    void CreateObstacle()
+    {
+        timer--;
+        Instantiate(rocks);
+        score+=5;
+    }
+
+   
+}
---
+++ hacktm/Assets/Scripts/Player.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    // The force which is added when the player jumps
+    // This can be changed in the Inspector window
+    //public Vector2 jumpForce = new Vector2(0, 300);
+    public Vector2 jumpForce = new Vector2(0, 300);
+    public Transform grow;
+    GameObject transformer;
+
+   
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {            
+            //Debug.Log(collision.gameObject.tag);            
+            grow.localScale += new Vector3(0.015f,0.015f, 0);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Jump
+        
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyUp("space"))
+        {
+            //GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            GetComponent<Rigidbody2D>().AddForce(jumpForce);
+        }
+
+        // Die by being off screen
+        Vector2 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+        if (screenPosition.y > Screen.height || screenPosition.y < 0 || screenPosition.x < -10)
+        {
+            Die();
+        }
+        
+        
+    }
+
+    // Die by collision
+    //void OnCollisionEnter2D(Collision2D other)
+    //{
+   //     Die();
+   // }
+
+    void Die()
+    {
+        //Application.LoadLevel(Application.loadedLevel);        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
