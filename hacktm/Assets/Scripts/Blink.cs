ff9bef29a45039cdd2209e2b0ceb7024ea6b5725
solidgriever <florentinbeltechi@gmail.com>
2017-06-02 18:58:03 +0300

    added some blinky colors to main menu & transitions

---
+++ hacktm/Assets/Scripts/Blink.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class Blink : MonoBehaviour
+{
+
+
+    public float timer2 = 0.0f;
+
+ 
+    // Update is called once per frame
+    void Update()
+	{
+
+		//Change ball color randomly
+		timer2 += Time.deltaTime;
+		if (timer2 >= 0.2f)//change the float value here to change how long it takes to switch.
+		{
+		 //pick a random color
+		     Color newColor = new Color(Random.value, Random.value, Random.value, 1.0f);
+		// apply it on current object's material
+		      gameObject.GetComponent<Image>().color = newColor;
+            
+		     timer2 = 0;
+		 }
+			
+}
+}
